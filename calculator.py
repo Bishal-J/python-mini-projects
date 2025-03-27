0b7d83adf1010241e57752fbdd606c42dbd84912
bishal-j <bishal120101@gmail.com>
2025-03-27 23:11:26 +0530

    calculator boilerplate setup

--- calculator.py
+++ calculator.py
@@ -1,10 +1,36 @@
 def run_calculator():
     print("Calculator====")
 
+def add(n1, n2):
+    return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
 
-def format_name(f_name, l_name):
-    print(f"{f_name.title()} {l_name.title()}")
+def divide(n1, n2):
+    return n1 / n2
 
-format_name('bIshAL', 'Adhikari')
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply, 
+    "/": divide
+}    
+
+num1 = int(input("Enter your first number: "))
+for key in operations:
+    print(key)
+operator = input("Pick an operation: ")
+num2 = int(input("Enter your second number: "))
+
+answer = operations[operator](num1, num2)
+
+print(f"{num1} {operator} {num2} = {answer}")
+
+choice = input("Type 'Y' to continue calculating with {answer}, or type 'N' " ).lower()
+
+if choice == 'y':
+    num1 = answer
